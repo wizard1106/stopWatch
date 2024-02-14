fedeb58be650d6f7b5b2cedb01603baceebe3c05
paul <paultadam5@outlook.com>
2024-02-14 10:03:45 -0600

    deleted unneeded stuff

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
-//import React, { useState } from 'react';
-//import { View, Text, Button } from 'react-native'
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
 
 const FlatListBasics = () => {
   const [time, setTime] = useState(0);
@@ -27,14 +16,5 @@ return(
   );
 
 };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 export default FlatListBasics;
