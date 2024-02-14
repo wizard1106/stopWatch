d1a2483f4010168b3764a7f543b44ac63cf5dedf
paul <paultadam5@outlook.com>
2024-02-14 10:14:53 -0600

    resolving merge errors

--- App.js
+++ App.js
@@ -17,13 +17,5 @@ return(
 
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 export default FlatListBasics;
